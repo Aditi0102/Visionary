40a494eded1c7209e5339980f79f4e81ceb8d84b
AvishrantsSh <avishrants@gmail.com>
2021-01-28 15:52:21 +0530

    Seperate Video Extractor Init

---
+++ Train/imgextract.py
@@ -0,0 +1,28 @@
+# A custom class for extracting image assets from videos
+import cv2, os
+from multiprocessing import Pool
+
+class Extractor(object):
+    count = 0
+    def __init__(self, FRAME_SIZE = (64, 64), FRAME_SKIP = 10):
+        self.size = FRAME_SIZE
+        self.skip = FRAME_SKIP
+
+    def extract(self, path, DIR = 'tmp'):
+        folder = "./Data/train/" + DIR
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        
+        cap = cv2.VideoCapture(path)   # capturing the video from the given path
+        while True:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, Extractor.count*self.skip)
+            frame = []
+            ret, frame = cap.read()
+            if ret is False:
+                break
+            
+            frame = cv2.resize(frame, self.size)
+            fname = '%s/%d.jpeg' % (folder, Extractor.count)
+            cv2.imwrite(fname, frame)
+            Extractor.count += 1
+        # cap.release()
