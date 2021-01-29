5f98f641b0a17868ab578b2ba8efc0ab59781060
AvishrantsSh <avishrants@gmail.com>
2021-01-29 13:21:20 +0530

    Extractor wrong count issue resolved

--- imgextract.py
+++ imgextract.py
@@ -3,10 +3,10 @@ import cv2, os
 from multiprocessing import Pool
 
 class Extractor(object):
-    count = 0
     def __init__(self, FRAME_SIZE = (64, 64), FRAME_SKIP = 10):
         self.size = FRAME_SIZE
         self.skip = FRAME_SKIP
+        self.count = 0
 
     def extract(self, path, DIR = 'tmp'):
         folder = "./Data/" + DIR
@@ -15,14 +15,14 @@ class Extractor(object):
         
         cap = cv2.VideoCapture(path)   # capturing the video from the given path
         while True:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, Extractor.count*self.skip)
+            cap.set(cv2.CAP_PROP_POS_FRAMES, self.count*self.skip)
             frame = []
             ret, frame = cap.read()
             if ret is False:
                 break
             
             frame = cv2.resize(frame, self.size)
-            fname = '%s/%d.jpeg' % (folder, Extractor.count)
+            fname = '%s/%d.jpeg' % (folder, self.count)
             cv2.imwrite(fname, frame)
-            Extractor.count += 1
+            self.count += 1
         # cap.release()
