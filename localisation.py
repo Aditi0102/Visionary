1b633b524abe404b0ce182569cfebd4dacb49bf4
Ritik dhiman <57491718+ritikdh440@users.noreply.github.com>
2021-01-29 23:03:29 +0530

    fast speed

--- localisation.py
+++ localisation.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import imutils
+import time
 
 # initialize the HOG descriptor/person detector
 hog = cv2.HOGDescriptor()
@@ -8,10 +9,18 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 cv2.startWindowThread()
 
 cap = cv2.VideoCapture("./Data/Dummy/1.mp4")
+
+frameCount = 0
+start_time = time.time()
  
 while(True):
 
-    ret, frame = cap.read()
+    ret, frame = cap.read() 
+    cv2.waitkey(30)
+    
+    #resizing for faster detection
+    frame = cv2.resize(frame, (640,480))
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
    
     # detect person in the image & returns the bounding boxes for the detected person
@@ -23,9 +32,9 @@ while(True):
         cv2.rectangle(frame, (xA, yA), (xB, yB),(0, 255, 0), 2)
 
     cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(30) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-cv2.waitKey(1)
+cv2.waitKey(30)
